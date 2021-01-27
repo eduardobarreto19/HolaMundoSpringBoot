ef74cee6661a50e135da489368794e9c949e7303
Luis Eduardo <75134763+luiseduardobarreto@users.noreply.github.com>
2021-01-27 14:21:40 -0500

    Hola Mundo Spring boot 2

---
+++ src/main/java/com/example/demo/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //exponer api rest
+public class HolaMundo {
+
+    @RequestMapping("/") //Indica que se ejecuta este metodo cuando visitamos la raiz
+    public String hola(){
+        return "hola mundo";
+    }
+}
